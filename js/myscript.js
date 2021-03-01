6fe74c6c28424e38ece8e143d7808afa94747880
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-03-01 18:34:35 +0100

    final

--- js/myscript.js
+++ js/myscript.js
@@ -1,19 +1,19 @@
-// var email = prompt("Inserisci la tua email");
-// var mails = ["mail1", "mail2", "mail3", "mail4"];
-// var isInArray = mails.includes(email);
-// if (isInArray == true) {
-//   document.getElementById("lista").innerHTML = "Sei";
-// } else {
-//   document.getElementById("lista").innerHTML = "Non sei";
-// }
-
-var mails = ["mail1", "mail2", "mail3", "mail4"];
 var email = prompt("Inserisci la tua email");
-for (var i = 0; i < mails.length; i++) {
-  if (mails[i] == email) {
-    document.getElementById("lista").innerHTML = "Sei";
-  }
+var mails = ["mail1", "mail2", "mail3", "mail4"];
+var isInArray = mails.includes(email);
+if (isInArray == true) {
+  document.getElementById("lista").innerHTML = "Sei";
+} else {
+  document.getElementById("lista").innerHTML = "Non sei";
 }
+
+// var mails = ["mail1", "mail2", "mail3", "mail4"];
+// var email = prompt("Inserisci la tua email");
+// for (var i = 0; i < mails.length; i++) {
+//   if (mails[i] == email) {
+//     document.getElementById("lista").innerHTML = "Sei";
+//   }
+// }
 
 
 
